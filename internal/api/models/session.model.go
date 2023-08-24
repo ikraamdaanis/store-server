d5b67cc7255f1a64c6f931123abfa2999c694668
Ikraam <ibndaanis@gmail.com>
2023-08-24 22:49:47 +0100

    expires at

--- internal/api/models/session.model.go
+++ internal/api/models/session.model.go
@@ -12,6 +12,7 @@ type Session struct {
 	Token      string    `gorm:"type:varchar(255);not null"`
 	UserAgent  string    `gorm:"type:varchar(255);not null"`
 	IP_Address string    `gorm:"type:varchar(255);not null"`
+	ExpiresAt  time.Time
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 }
